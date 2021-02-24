ee648c9bc02bf4e375ee495625b223e25a3de48a
urmilshroff <urmilshroff@gmail.com>
2021-02-24 12:26:52 +0530

    Add :coffee:

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var description = new Typed('#description', {
-        strings: ['Tech Enthusiast^2500', 'App Developer^2500', 'Computer Engineer^2500', 'Public Speaker^2500', 'Open Source Contributor^2500', 'YouTube Creator^2500', 'Petrolhead^2500', 'Music Lover^2500'],
+        strings: ['Tech Enthusiast^2500', 'App Developer^2500', 'Computer Engineer^2500', 'Coffee Enthusiast^2500', 'Open Source Contributor^2500', 'YouTube Creator^2500', 'Petrolhead^2500', 'Music Lover^2500'],
         shuffle: true,
         loop: true,
         typeSpeed: 100,
